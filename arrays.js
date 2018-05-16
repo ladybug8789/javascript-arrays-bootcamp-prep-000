ed3866d5f784dedc92e7fa8326c0956dcfb9129d
Sara Ledger <sara.ledger@live.com>
2018-05-16 22:07:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, "mounds") {
+    chocolateBars.unshift("mounds")
+    return chocolateBars
+}
