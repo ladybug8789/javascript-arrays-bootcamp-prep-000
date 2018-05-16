8fc1be5c4b507320e8c83d3c033b05b7d74467e5
Sara Ledger <sara.ledger@live.com>
2018-05-16 22:00:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var array = ['1']
+  ['2', ...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var chocolatBars = ['mounds', ...chocolateBars],
+  return chocolateBars
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
